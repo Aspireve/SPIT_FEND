16d9c3d2c89c125629af52f4193d0a64750af559
Dinesh Kharah <dineshkharahcodes@gmail.com>
2024-11-10 04:36:49 +0530

    Marketplace done

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces (not just localhost)
+    port: 5173,        // Port you're running the app on (default for Vite)
+  },
   plugins: [react()],
   resolve: {
     alias: {
